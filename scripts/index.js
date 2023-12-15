d52d0ae15d146f38124c6f7fa6eb42a0ad382df1
RishaanGupta <118270242+RishaanGupta@users.noreply.github.com>
2023-12-15 16:23:49 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 16, 2020',
+  birthdate: 'Dec 16, 2023',
   name: 'Mommy'
 };
 
